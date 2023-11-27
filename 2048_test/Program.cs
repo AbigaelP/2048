946632926b6aac6373fa6f5b54926248ecf79f75
AbigaelP <150002397+AbigaelP@users.noreply.github.com>
2023-11-27 10:09:42 +0100

    Générer 2 nombre début de partie

--- 2048_test/Program.cs
+++ 2048_test/Program.cs
@@ -8,20 +8,24 @@ namespace _2048_test
 {
     internal class Program
     {
+        //définir l'aléatoire en dehors du void main pour gagner de la rapidité
+        static Random random = new Random();
         static void Main(string[] args)
         {
-
             //Initialisé le tableau
             int[,] tableau2D = new int[4, 4];
 
             // Créer une variable booléenne initialisée à true
-            bool mouve = true;
+            bool jeu = true;
 
-            // Afficher le tableau 2D
-            AfficherTableau(tableau2D);
+            //mettre deux nombre aléatoire
+            for (int p =0; p <=1; p++)
+            {
+                aleatoire(tableau2D);
+            }
 
             // Une boucle while qui continue tant qu'il reste des 0 dans le tableau
-            while (mouve && controle(tableau2D) == true)
+            while (jeu && controle(tableau2D) == true)
             {
                 //Détecter sur quelle touche l'utilisateur appuie
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
@@ -32,19 +36,23 @@ namespace _2048_test
                 {
                     case ConsoleKey.UpArrow:
                         //ici fonction du mouvemnets des tuiles
+                        //ici fonction généré 1 nombre éléatoire
                         break;
                     case ConsoleKey.DownArrow:
                         //ici fonction du mouvemnets des tuiles
+                        //ici fonction généré 1 nombre éléatoire
                         break;
                     case ConsoleKey.LeftArrow:
                         //ici fonction du mouvemnets des tuiles
+                        //ici fonction généré 1 nombre éléatoire
                         break;
                     case ConsoleKey.RightArrow:
                         //ici fonction du mouvemnets des tuiles
+                        //ici fonction généré 1 nombre éléatoire
                         break;
                     case ConsoleKey.C:
                         // Arrêter le programme si la touche C est pressée
-                        mouve = false;
+                        jeu = false;
                         Environment.Exit(0);
                         break;
                     default:
@@ -57,7 +65,47 @@ namespace _2048_test
             Console.ReadKey();
         }
 
-        // fonction booléenne qui controle s'il reste un 0 dans le tableau
+        //fonction générer un nombre aléatoire dans le tableau
+        static void aleatoire(int[,] tableau)
+        {
+            int ligne = tableau.GetLength(0);
+            int colone = tableau.GetLength(1);
+
+            int aleatoireLigne = random.Next(0, 4);
+            int aleatoireColone = random.Next(0, 4);
+            int nombreAleatoire = random.Next(0, 10);
+
+            //met un chiffre aléatoire uniquement s'il ecrase un 0
+            if (tableau[aleatoireLigne, aleatoireColone] == 0)
+            {
+                if (nombreAleatoire == 9)
+                {
+                    tableau[aleatoireLigne, aleatoireColone] = 4;
+                }
+                else
+                {
+                    tableau[aleatoireLigne, aleatoireColone] = 2;
+                }
+               
+
+                Console.Clear();
+                Console.WriteLine();
+
+                AfficherTableau(tableau);
+
+                
+
+                Console.WriteLine();
+            }
+
+            //le chiffre écrasé n'est pas un 0 alors on recommence au début de la fonction
+            else
+            {
+               aleatoire(tableau);
+            }
+        }
+
+        // Fonction booléenne qui controle s'il reste un 0 dans le tableau
         static bool controle(int[,] tableau)
         {
             int ligne = tableau.GetLength(0);
@@ -80,7 +128,7 @@ namespace _2048_test
             return false;
         }
 
-        // Méthode pour afficher un tableau 2D
+        // Fonction pour afficher un tableau 2D
         static void AfficherTableau(int[,] tableau)
         {
             int ligne = tableau.GetLength(0);
