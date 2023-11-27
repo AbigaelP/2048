a05b517e5fbfc89390e19fb15125f9c04b4423b2
AbigaelP <150002397+AbigaelP@users.noreply.github.com>
2023-11-27 11:25:10 +0100

    Test déplacement gauche

--- 2048_test/Program.cs
+++ 2048_test/Program.cs
@@ -10,6 +10,7 @@ namespace _2048_test
     {
         //définir l'aléatoire en dehors du void main pour gagner de la rapidité
         static Random random = new Random();
+
         static void Main(string[] args)
         {
             //Initialisé le tableau
@@ -44,6 +45,7 @@ namespace _2048_test
                         break;
                     case ConsoleKey.LeftArrow:
                         //ici fonction du mouvemnets des tuiles
+                        gauche(tableau2D);
                         //ici fonction généré 1 nombre éléatoire
                         break;
                     case ConsoleKey.RightArrow:
@@ -65,35 +67,82 @@ namespace _2048_test
             Console.ReadKey();
         }
 
+        //fonction déplacer les chiffres à gauche
+        static void gauche(int[,] tableau)
+        {
+            int[] gauche = new int[4];
+
+            for (int ligne =0; ligne<=tableau.GetLength(0); ligne++)
+            {
+                for (int colone = 0; colone <= tableau.GetLength(1); colone++)
+                {
+                    gauche[colone]= tableau[ligne, colone];
+                }
+                for(int x = 0; x < tableau.GetLength(1); x++)
+                {
+                    tableau[ligne,x]= changerOrdreGauche(gauche[0], gauche[1], gauche[2], gauche[3]);
+                }
+             
+            }
+            
+        }
+
+        //Fonction qui change l'ordre des nombres à gauche
+        static int[] changerOrdreGauche(int nombreZero, int nombreUn, int nombreDeux, int nombreTrois)
+        {
+
+            if (nombreDeux == 0 && nombreTrois > 0)
+            {
+                nombreDeux = nombreTrois;
+                nombreTrois = 0;
+            }
+
+            if (nombreUn == 0 && nombreDeux > 0)
+            {
+                nombreUn = nombreDeux;
+                nombreDeux = nombreTrois;
+                nombreTrois = 0;
+            }
+
+            if (nombreZero == 0 && nombreUn > 0)
+            {
+                nombreZero = nombreUn;
+                nombreUn = nombreDeux;
+                nombreDeux = nombreTrois;
+                nombreTrois = 0;
+            }
+            int[] i = { nombreZero, nombreUn, nombreDeux, nombreTrois };
+            return i;
+        }
+
         //fonction générer un nombre aléatoire dans le tableau
         static void aleatoire(int[,] tableau)
         {
             int ligne = tableau.GetLength(0);
             int colone = tableau.GetLength(1);
 
-            int aleatoireLigne = random.Next(0, 4);
-            int aleatoireColone = random.Next(0, 4);
-            int nombreAleatoire = random.Next(0, 10);
+            int aleatoireLigne = random.Next(0, 4);  //ligne aléatoire du tableau
+            int aleatoireColone = random.Next(0, 4);  //colone aléatoire du tableau
+            int nombreAleatoire = random.Next(0, 10);  //généré un nombre aléatoire
 
             //met un chiffre aléatoire uniquement s'il ecrase un 0
             if (tableau[aleatoireLigne, aleatoireColone] == 0)
             {
+
+                //choix d'afficher un 2 ou un 4 avec 90% et 10% d chance
                 if (nombreAleatoire == 9)
                 {
-                    tableau[aleatoireLigne, aleatoireColone] = 4;
+                    tableau[aleatoireLigne, aleatoireColone] = 4;         //affiche un 4 dans le tableau
                 }
                 else
                 {
-                    tableau[aleatoireLigne, aleatoireColone] = 2;
+                    tableau[aleatoireLigne, aleatoireColone] = 2;         //affiche un 2 dans le tableau
                 }
                
-
                 Console.Clear();
                 Console.WriteLine();
 
                 AfficherTableau(tableau);
-
-                
 
                 Console.WriteLine();
             }
